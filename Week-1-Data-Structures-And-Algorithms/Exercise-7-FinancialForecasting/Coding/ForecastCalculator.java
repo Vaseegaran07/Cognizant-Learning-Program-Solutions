908fb15606a2c862b51d798d66dc702740b531c4
Vaseegaran07 <118419574+Vaseegaran07@users.noreply.github.com>
2025-06-21 13:31:45 +0530

    Create ForecastCalculator.java

---
+++ Week-1-Data-Structures-And-Algorithms/Exercise-7-FinancialForecasting/Coding/ForecastCalculator.java
@@ -0,0 +1,12 @@
+package forecasting;
+
+public class ForecastCalculator {
+
+    public static double calculateFutureValue(double initialAmount, double growthRate, int years) {
+        if (years == 0) {
+            return initialAmount;
+        }
+
+        return calculateFutureValue(initialAmount, growthRate, years - 1) * (1 + growthRate);
+    }
+}
