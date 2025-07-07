f82b0f8a252f36c8affa8826a379d44a4f3794ae
Vaseegaran07 <118419574+Vaseegaran07@users.noreply.github.com>
2025-07-07 18:58:39 +0530

    Create Employee.java

---
+++ Week-3-Spring-Data-JPA-With-Spring-Boot-Hibernate/Difference-Between-JPA-Hibernate-And-Spring-Data-JPA/Coding/Employee.java
@@ -0,0 +1,24 @@
+package com.example.jpa_demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String name;
+  private String department;
+
+  public Integer getId() { return id; }
+  public void setId(Integer id) { this.id = id; }
+
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+
+  public String getDepartment() { return department; }
+  public void setDepartment(String department) { this.department = department; }
+}
